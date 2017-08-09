6e6e66a3bec771a8c9b81bd633da9bc9aed0c99e
papercut1234 <nao.public@gmail.com>
2017-08-10 00:45:09 +0900

    test

---
+++ Proxy/Proxy/Subject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Proxy
+{
+    public abstract class Subject
+    {
+        protected String name;
+        public abstract void request1(String name);
+        public abstract void request2();
+        public abstract void request3();
+
+    }
+}
---
+++ Proxy2/Proxy2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Proxy2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
